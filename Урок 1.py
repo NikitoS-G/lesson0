69a01b785215c7ee187f3a66d274084fc44627b3
NikitoS-G <nikitosg3518@gmail.com>
2024-11-12 00:13:58 +0700

    Add files via upload

---
+++ Урок 1.py
@@ -0,0 +1,17 @@
+from lesson0.main import print_hi
+print(5)# «integer», что переводится, как «целое число»
+print(type(5)) # Соответственно целые числа относятся к типу данных int
+print(5 // 5) # // - Целочисленное деление, / - деление
+print(5 ** 5) # ** - Возведение в степень
+print(2.0) # 2.0 - дробным числам
+print(type(2.0)) # Дробные числа относятся к типу «float»
+print(2.0 + 2)
+print('hello world')
+print(type('hello world')) # string - строка
+print('1'+'1')
+print(True,False)#  булевый тип данных (boolean)
+print(type(True),type(False)) # True и False, то есть верно и неверно
+print(5 > 10, 10 > 5)
+print(5 != 5)
+print(5 == 5 and 5 < 10) # Команды: and - и, or - или.
+print(type(int('5')))
